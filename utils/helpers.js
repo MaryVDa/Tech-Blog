20e12282dbc53e8e21969155eb05b8c2a88ee679
Mary Dault <bluestar1dault@gmail.com>
2023-05-31 10:49:14 -0400

    helpers utils

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+module.exports = {
+    format_time: (date) => {
+        //format date as MM/DD/YYYY
+        return date.toLocaleDateString();
+    },
+    format_date: (date) => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+};
