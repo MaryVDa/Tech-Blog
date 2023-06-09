7712523b53b160ded0be3100d157c25e1720c2d7
Mary Dault <bluestar1dault@gmail.com>
2023-06-09 09:40:41 -0400

    add delete comment in routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -28,4 +28,34 @@ router.post('/', withAuth, async(req, res) => {
     }
 });
 
+router.put('/:id', withAuth, async(req, res) => {
+    try {
+        const commentData = await Comment.update({
+            ...req.body,
+            userId: req.session.userId,
+        });
+        res.json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async(req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                userId: req.session.userId,
+            },
+        });
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id!' });
+            return;
+        }
+        res.json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
