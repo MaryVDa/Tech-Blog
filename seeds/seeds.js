b0e3144f19d6a16806f76045c4b1c8d9fbcb3358
Mary Dault <bluestar1dault@gmail.com>
2023-06-09 15:15:38 -0400

    update seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -18,6 +19,15 @@ const seedDatabase = async () => {
             user_id: users[Math.floor(Math.random() * users.length)].id,
         });
     }
+
+    for (const comment of commentData) {
+        await Comment.create({
+            ...comment,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+            post_id: Math.floor(Math.random() * postData.length) + 1,
+        });
+    }
+    
     process.exit(0);
 };
 
